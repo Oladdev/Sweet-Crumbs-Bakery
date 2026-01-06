4f0b1c0ce40968f92e702ddd6bf65a070c285272
Oladdev <pwreshy@gmail.com>
2026-01-06 22:35:24 +0100

    add gallery.js with image sources for bakery products

---
+++ assets/js/gallery.js
@@ -0,0 +1,30 @@
+const breadSources = [
+    "../assets/Images/Sourdough Bread.jpg",
+    "../assets/Images/Baguette.jpg",
+];
+
+const pasterySources = [
+"../assets/Images/Croissant.jpeg",
+"../assets/Images/Danish.jpeg",
+];
+
+const cupcakeSources = [
+"../assets/Images/choclate cake.jpeg",
+"../assets/Images/vanilla cupcake.jpg",
+"../assets/Images/cherrycc.jpeg",
+"../assets/Images/cupcake.jpeg",
+];
+
+const cakeSources = [
+"../assets/Images/choclate cake.jpeg",
+"../assets/Images/Lemon cake.jpeg",
+"../assets/Images/Drip cake.jpg",
+"../assets/Images/Oreo drip cake.jpg",
+"../assets/Images/goat cake.jpg",
+"../assets/Images/Pink cat cake.jpg",
+];
+
+const beverageSources = [
+"../assets/Images/Artisan coffee.jpeg",
+"../assets/Images/Herbal Tea.jpeg",
+];
